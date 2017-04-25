e467e8a503066feb866cff2ce24856cf54776531
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-04-25 15:32:00 +0200

    Switches to ava

--- test/lib/createClient-test.js
+++ test/lib/createClient-test.js
@@ -1,74 +1,67 @@
 'use strict'
 
-var tap = require('tap')
-var createClient = require('../../lib/createClient')
-var masterOptions = require('./../data/options.json').config
+const test = require('ava')
+const createClient = require('../../lib/createClient')
+const masterOptions = require('./../data/options.json').config
 
-tap.test('It requires an options object to be supplied', function (test) {
-  var options = false
-  var expectedErrorMessage = 'Missing required input: options object'
-  createClient(options, function (error, data) {
-    test.equal(error.message, expectedErrorMessage, expectedErrorMessage)
-    test.done()
+test('It requires an options object to be supplied', t => {
+  const options = false
+  const expectedErrorMessage = 'Missing required input: options object'
+  createClient(options, (error, data) => {
+    t.deepEqual(error.message, expectedErrorMessage, expectedErrorMessage)
   })
 })
 
-tap.test('requires param options.url to exist', function (test) {
-  var options = JSON.parse(JSON.stringify(masterOptions))
-  var expectedErrorMessage = 'Missing required input: options.url'
+test('requires param options.url to exist', t => {
+  let options = JSON.parse(JSON.stringify(masterOptions))
+  const expectedErrorMessage = 'Missing required input: options.url'
   options.url = false
-  createClient(options, function (error, data) {
-    test.equal(error.message, expectedErrorMessage, expectedErrorMessage)
-    test.done()
+  createClient(options, (error, data) => {
+    t.deepEqual(error.message, expectedErrorMessage, expectedErrorMessage)
   })
 })
 
-tap.test('requires param namespaceBrukersesjon to exist', function (test) {
-  var options = JSON.parse(JSON.stringify(masterOptions))
-  var expectedErrorMessage = 'Missing required input: options.namespaceBrukersesjon'
+test('requires param namespaceBrukersesjon to exist', t => {
+  let options = JSON.parse(JSON.stringify(masterOptions))
+  const expectedErrorMessage = 'Missing required input: options.namespaceBrukersesjon'
   options.namespaceBrukersesjon = false
-  createClient(options, function (error, data) {
-    test.equal(error.message, expectedErrorMessage, expectedErrorMessage)
-    test.done()
+  createClient(options, (error, data) => {
+    t.deepEqual(error.message, expectedErrorMessage, expectedErrorMessage)
   })
 })
 
-tap.test('requires param options.distribusjonskanal to exist', function (test) {
-  var options = JSON.parse(JSON.stringify(masterOptions))
-  var expectedErrorMessage = 'Missing required input: options.distribusjonskanal'
+test('requires param options.distribusjonskanal to exist', t => {
+  let options = JSON.parse(JSON.stringify(masterOptions))
+  const expectedErrorMessage = 'Missing required input: options.distribusjonskanal'
   options.distribusjonskanal = false
-  createClient(options, function (error, data) {
-    test.equal(error.message, expectedErrorMessage, expectedErrorMessage)
-    test.done()
+  createClient(options, (error, data) => {
+    t.deepEqual(error.message, expectedErrorMessage, expectedErrorMessage)
   })
 })
 
-tap.test('requires param options.systemnavn to exist', function (test) {
-  var options = JSON.parse(JSON.stringify(masterOptions))
-  var expectedErrorMessage = 'Missing required input: options.systemnavn'
+test('requires param options.systemnavn to exist', t => {
+  let options = JSON.parse(JSON.stringify(masterOptions))
+  const expectedErrorMessage = 'Missing required input: options.systemnavn'
   options.systemnavn = false
-  createClient(options, function (error, data) {
-    test.equal(error.message, expectedErrorMessage, expectedErrorMessage)
-    test.done()
+  createClient(options, (error, data) => {
+    t.deepEqual(error.message, expectedErrorMessage, expectedErrorMessage)
   })
 })
 
-tap.test('requires param options.brukernavn to exist', function (test) {
-  var options = JSON.parse(JSON.stringify(masterOptions))
-  var expectedErrorMessage = 'Missing required input: options.brukernavn'
+test('requires param options.brukernavn to exist', t => {
+  let options = JSON.parse(JSON.stringify(masterOptions))
+  const expectedErrorMessage = 'Missing required input: options.brukernavn'
   options.brukernavn = false
   createClient(options, function (error, data) {
-    test.equal(error.message, expectedErrorMessage, expectedErrorMessage)
-    test.done()
+    t.deepEqual(error.message, expectedErrorMessage, expectedErrorMessage)
   })
 })
 
-tap.test('requires param options.passord to exist', function (test) {
-  var options = JSON.parse(JSON.stringify(masterOptions))
-  var expectedErrorMessage = 'Missing required input: options.passord'
+test('requires param options.passord to exist', t => {
+  let options = JSON.parse(JSON.stringify(masterOptions))
+  const expectedErrorMessage = 'Missing required input: options.passord'
   options.passord = false
-  createClient(options, function (error, data) {
-    test.equal(error.message, expectedErrorMessage, expectedErrorMessage)
-    test.done()
+  createClient(options, (error, data) => {
+    t.deepEqual(error.message, expectedErrorMessage, expectedErrorMessage)
   })
 })
--- test/lib/node-dsf-test.js
+++ test/lib/node-dsf-test.js
@@ -1,144 +1,130 @@
 'use strict'
 
-var tap = require('tap')
-var dsf = require('../../index')
-var masterOptions = require('./../data/options.json')
+const test = require('ava')
+const dsf = require('../../index')
+let masterOptions = require('./../data/options.json')
 
-tap.test('It requires an options object to be supplied', function (test) {
-  var options = false
-  var expectedErrorMessage = 'Missing required input: options object'
-  dsf(options, function (error, data) {
-    test.equal(error.message, expectedErrorMessage, expectedErrorMessage)
-    test.done()
+test('It requires an options object to be supplied', t => {
+  const options = false
+  const expectedErrorMessage = 'Missing required input: options object'
+  dsf(options, (error, data) => {
+    t.deepEqual(error.message, expectedErrorMessage, expectedErrorMessage)
   })
 })
 
-tap.test('requires param options.config to exist', function (test) {
-  var options = JSON.parse(JSON.stringify(masterOptions))
-  var expectedErrorMessage = 'Missing required input: options.config'
+test('requires param options.config to exist', t => {
+  let options = JSON.parse(JSON.stringify(masterOptions))
+  const expectedErrorMessage = 'Missing required input: options.config'
   options.config = false
-  dsf(options, function (error, data) {
-    test.equal(error.message, expectedErrorMessage, expectedErrorMessage)
-    test.done()
+  dsf(options, (error, data) => {
+    t.deepEqual(error.message, expectedErrorMessage, expectedErrorMessage)
   })
 })
 
-tap.test('requires param options.config.url to exist', function (test) {
-  var options = JSON.parse(JSON.stringify(masterOptions))
-  var expectedErrorMessage = 'Missing required input: options.config.url'
+test('requires param options.config.url to exist', t => {
+  let options = JSON.parse(JSON.stringify(masterOptions))
+  const expectedErrorMessage = 'Missing required input: options.config.url'
   options.config.url = false
-  dsf(options, function (error, data) {
-    test.equal(error.message, expectedErrorMessage, expectedErrorMessage)
-    test.done()
+  dsf(options, (error, data) => {
+    t.deepEqual(error.message, expectedErrorMessage, expectedErrorMessage)
   })
 })
 
-tap.test('requires param options.config.namespaceBrukersesjon to exist', function (test) {
-  var options = JSON.parse(JSON.stringify(masterOptions))
-  var expectedErrorMessage = 'Missing required input: options.config.namespaceBrukersesjon'
+test('requires param options.config.namespaceBrukersesjon to exist', t => {
+  let options = JSON.parse(JSON.stringify(masterOptions))
+  const expectedErrorMessage = 'Missing required input: options.config.namespaceBrukersesjon'
   options.config.namespaceBrukersesjon = false
-  dsf(options, function (error, data) {
-    test.equal(error.message, expectedErrorMessage, expectedErrorMessage)
-    test.done()
+  dsf(options, (error, data) => {
+    t.deepEqual(error.message, expectedErrorMessage, expectedErrorMessage)
   })
 })
 
-tap.test('requires param options.config.distribusjonskanal to exist', function (test) {
-  var options = JSON.parse(JSON.stringify(masterOptions))
-  var expectedErrorMessage = 'Missing required input: options.config.distribusjonskanal'
+test('requires param options.config.distribusjonskanal to exist', t => {
+  let options = JSON.parse(JSON.stringify(masterOptions))
+  const expectedErrorMessage = 'Missing required input: options.config.distribusjonskanal'
   options.config.distribusjonskanal = false
-  dsf(options, function (error, data) {
-    test.equal(error.message, expectedErrorMessage, expectedErrorMessage)
-    test.done()
+  dsf(options, (error, data) => {
+    t.deepEqual(error.message, expectedErrorMessage, expectedErrorMessage)
   })
 })
 
-tap.test('requires param options.config.systemnavn to exist', function (test) {
-  var options = JSON.parse(JSON.stringify(masterOptions))
-  var expectedErrorMessage = 'Missing required input: options.config.systemnavn'
+test('requires param options.config.systemnavn to exist', t => {
+  let options = JSON.parse(JSON.stringify(masterOptions))
+  const expectedErrorMessage = 'Missing required input: options.config.systemnavn'
   options.config.systemnavn = false
-  dsf(options, function (error, data) {
-    test.equal(error.message, expectedErrorMessage, expectedErrorMessage)
-    test.done()
+  dsf(options, (error, data) => {
+    t.deepEqual(error.message, expectedErrorMessage, expectedErrorMessage)
   })
 })
 
-tap.test('requires param options.config.brukernavn to exist', function (test) {
-  var options = JSON.parse(JSON.stringify(masterOptions))
-  var expectedErrorMessage = 'Missing required input: options.config.brukernavn'
+test('requires param options.config.brukernavn to exist', t => {
+  let options = JSON.parse(JSON.stringify(masterOptions))
+  const expectedErrorMessage = 'Missing required input: options.config.brukernavn'
   options.config.brukernavn = false
-  dsf(options, function (error, data) {
-    test.equal(error.message, expectedErrorMessage, expectedErrorMessage)
-    test.done()
+  dsf(options, (error, data) => {
+    t.deepEqual(error.message, expectedErrorMessage, expectedErrorMessage)
   })
 })
 
-tap.test('requires param options.config.passord to exist', function (test) {
-  var options = JSON.parse(JSON.stringify(masterOptions))
-  var expectedErrorMessage = 'Missing required input: options.config.passord'
+test('requires param options.config.passord to exist', t => {
+  let options = JSON.parse(JSON.stringify(masterOptions))
+  const expectedErrorMessage = 'Missing required input: options.config.passord'
   options.config.passord = false
-  dsf(options, function (error, data) {
-    test.equal(error.message, expectedErrorMessage, expectedErrorMessage)
-    test.done()
+  dsf(options, (error, data) => {
+    t.deepEqual(error.message, expectedErrorMessage, expectedErrorMessage)
   })
 })
 
-tap.test('requires param options.method to exist', function (test) {
-  var options = JSON.parse(JSON.stringify(masterOptions))
-  var expectedErrorMessage = 'Missing required input: options.method'
+test('requires param options.method to exist', t => {
+  let options = JSON.parse(JSON.stringify(masterOptions))
+  const expectedErrorMessage = 'Missing required input: options.method'
   options.method = false
-  dsf(options, function (error, data) {
-    test.equal(error.message, expectedErrorMessage, expectedErrorMessage)
-    test.done()
+  dsf(options, (error, data) => {
+    t.deepEqual(error.message, expectedErrorMessage, expectedErrorMessage)
   })
 })
 
-tap.test('requires param options.query to exist', function (test) {
-  var options = JSON.parse(JSON.stringify(masterOptions))
-  var expectedErrorMessage = 'Missing required input: options.query'
+test('requires param options.query to exist', t => {
+  let options = JSON.parse(JSON.stringify(masterOptions))
+  const expectedErrorMessage = 'Missing required input: options.query'
   options.query = false
-  dsf(options, function (error, data) {
-    test.equal(error.message, expectedErrorMessage, expectedErrorMessage)
-    test.done()
+  dsf(options, (error, data) => {
+    t.deepEqual(error.message, expectedErrorMessage, expectedErrorMessage)
   })
 })
 
-tap.test('requires param options.query.saksref to exist', function (test) {
-  var options = JSON.parse(JSON.stringify(masterOptions))
-  var expectedErrorMessage = 'Missing required input: options.query.saksref'
+test('requires param options.query.saksref to exist', t => {
+  let options = JSON.parse(JSON.stringify(masterOptions))
+  const expectedErrorMessage = 'Missing required input: options.query.saksref'
   options.query.saksref = false
-  dsf(options, function (error, data) {
-    test.equal(error.message, expectedErrorMessage, expectedErrorMessage)
-    test.done()
+  dsf(options, (error, data) => {
+    t.deepEqual(error.message, expectedErrorMessage, expectedErrorMessage)
   })
 })
 
-tap.test('requires param options.query.foedselsnr to exist', function (test) {
-  var options = JSON.parse(JSON.stringify(masterOptions))
-  var expectedErrorMessage = 'Missing required input: options.query.foedselsnr'
+test('requires param options.query.foedselsnr to exist', t => {
+  let options = JSON.parse(JSON.stringify(masterOptions))
+  const expectedErrorMessage = 'Missing required input: options.query.foedselsnr'
   options.query.foedselsnr = false
-  dsf(options, function (error, data) {
-    test.equal(error.message, expectedErrorMessage, expectedErrorMessage)
-    test.done()
+  dsf(options, (error, data) => {
+    t.deepEqual(error.message, expectedErrorMessage, expectedErrorMessage)
   })
 })
 
-tap.test('requires param options.query.etternavn to exist if options.query.fornavn is supplied', function (test) {
-  var options = JSON.parse(JSON.stringify(masterOptions))
-  var expectedErrorMessage = 'Missing required input: options.query.etternavn'
+test('requires param options.query.etternavn to exist if options.query.fornavn is supplied', t => {
+  let options = JSON.parse(JSON.stringify(masterOptions))
+  const expectedErrorMessage = 'Missing required input: options.query.etternavn'
   options.query.etternavn = false
-  dsf(options, function (error, data) {
-    test.equal(error.message, expectedErrorMessage, expectedErrorMessage)
-    test.done()
+  dsf(options, (error, data) => {
+    t.deepEqual(error.message, expectedErrorMessage, expectedErrorMessage)
   })
 })
 
-tap.test('requires param options.query.fornavn to exist if options.query.etternavn is supplied', function (test) {
-  var options = JSON.parse(JSON.stringify(masterOptions))
-  var expectedErrorMessage = 'Missing required input: options.query.fornavn'
+test('requires param options.query.fornavn to exist if options.query.etternavn is supplied', t => {
+  let options = JSON.parse(JSON.stringify(masterOptions))
+  const expectedErrorMessage = 'Missing required input: options.query.fornavn'
   options.query.fornavn = false
-  dsf(options, function (error, data) {
-    test.equal(error.message, expectedErrorMessage, expectedErrorMessage)
-    test.done()
+  dsf(options, (error, data) => {
+    t.deepEqual(error.message, expectedErrorMessage, expectedErrorMessage)
   })
 })
